08de4a09bf59a3256c4f836dc5bdf6d1ffd11f23
Jonas <jonas147781@gmail.com>
2024-03-11 11:30:29 +0100

    Constructor til DagligSkaev

--- src/ordination/DagligSkaev.java
+++ src/ordination/DagligSkaev.java
@@ -1,12 +1,30 @@
 package ordination;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
 
-public class DagligSkaev {
+public class DagligSkaev extends Ordination {
     // TODO
-
+    public DagligSkaev(LocalDate startDen, LocalDate slutDen, Laegemiddel laegemiddel) {
+        super(startDen, slutDen, laegemiddel);
+    }
 
     public void opretDosis(LocalTime tid, double antal) {
         // TODO
+    }
+
+    @Override
+    public double samletDosis() {
+        return 0;
+    }
+
+    @Override
+    public double doegnDosis() {
+        return 0;
+    }
+
+    @Override
+    public String getType() {
+        return null;
     }
 }
