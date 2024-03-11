05204677287b177ae4894228414047db0bab1af6
Jonas <jonas147781@gmail.com>
2024-03-11 09:51:03 +0100

    DagligFast lavet.. måske?

--- src/ordination/DagligFast.java
+++ src/ordination/DagligFast.java
@@ -1,5 +1,16 @@
 package ordination;
 
+import java.util.ArrayList;
+
 public class DagligFast {
     // TODO
+    private final ArrayList<Dosis> dosis = new ArrayList<>();
+    private double antal;
+
+    public ArrayList<Dosis> getDosis() {
+        return new ArrayList<>(dosis);
+    }
+    public double getAntal() {
+        return antal;
+    }
 }
